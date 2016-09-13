04a9c5070139030c38a580ed96264325cf9713be
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-13 11:04:48 -0500

    formated routes

---
+++ app/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var app = angular.module("Roomies", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+  when("/", {
+    templateUrl: "landingPage.html",
+    controller: "LandingPageCtrl"
+  }).
+  when("/registerHome", {
+    templateUrl: "register-home.html",
+    controller: "NewHomeCtrl"
+  }).
+  when("/profile/:homeid", {
+    templateUrl: "home-profile.html",
+    controller: "HomeViewCtrl"
+  }).
+  when("/:homeid", {
+    templateUrl: "home-tools.html",
+    controller: "HomeToolCtrl"
+  }).
+  when("/grocerylist/:homeid", {
+    templateUrl: "grocery-list.html",
+    controller: "GroceryViewCtrl"
+  }).
+  when("budget/:homeid", {
+    templateUrl: "budget-list.html",
+    controller: "BudgetViewCtrl"
+  }).
+  when("chores/:homeid", {
+    templateUrl: "chores-list.html",
+    controller: "ChoresViewCtrl"
+  }).
+  when("messages/:userid", {
+    templateUrl: "message-list.html",
+    controller: "MessageViewCtrl"
+  }).
+  when("invite-housemate/:userid", {
+    templateUrl: "invite-housemate.html",
+    controller: "NewHouseMateCtrl"
+  }).
+  otherwise('/');
+});
