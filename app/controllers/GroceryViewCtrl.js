4456c3b2049814553d11f6f5129b673cea6dac57
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-14 11:19:57 -0500

    can now add items to grocery list and grocery list will be loaded upon inital arrival to page

--- app/controllers/GroceryViewCtrl.js
+++ app/controllers/GroceryViewCtrl.js
@@ -2,6 +2,7 @@ app.controller("GroceryViewCtrl", function($scope, $window, GroceryFactory, Home
 
   let _homeid = HomeFactory.getHouseid();
   let _uid = AuthFactory.getUid();
+  let filteredGroceryArray = [];
 
   $scope.newGroceryItem = {
     "name": "",
@@ -15,23 +16,31 @@ app.controller("GroceryViewCtrl", function($scope, $window, GroceryFactory, Home
     GroceryFactory.newGroceryItem($scope.newGroceryItem)
     .then(function() {
       $window.location.href = `#/grocerylist/${HomeFactory.getHouseid()}`
-    })
+    });
+  };
+
+  $scope.getGroceryList = function() {
+    GroceryFactory.getGroceryList(_homeid).then( function(filteredGroceryArray) {
+      console.log("GVC filteredGroceryArray", filteredGroceryArray);
+      $scope.groceryList = filteredGroceryArray;
+      console.log()
+    });
   }
 
-    $scope.groceryList = [{
-        name: "Pancakes",
-        purchased: 'false',
-        spoiled: false
-    }, {
-      name: "Apples",
-      purchased: 'false',
-      spoiled: false
-    },
-    {
-      name: "Bananas",
-      purchased: 'true',
-      spoiled: false
-    },
-  ];
+  //   $scope.groceryList = [{
+  //       name: "Pancakes",
+  //       purchased: 'false',
+  //       spoiled: false
+  //   }, {
+  //     name: "Apples",
+  //     purchased: 'false',
+  //     spoiled: false
+  //   },
+  //   {
+  //     name: "Bananas",
+  //     purchased: 'true',
+  //     spoiled: false
+  //   },
+  // ];
 
 });
--- app/factories/GroceryFactory.js
+++ app/factories/GroceryFactory.js
@@ -2,15 +2,37 @@
 
 app.factory("GroceryFactory", function($q, $http, FBCreds, FirebaseURL) {
 
+  // let getGroceryList = (houseId) => {
+  //   return $q( (resolve, reject) => {
+  //     $http.get(`${FirebaseURL}/grocery.json`)
+  //     .then((data => {
+  //       console.log("data.data", data.data);
+  //       let groceryArray = convertResultsToArray(data.data, 'houseId', houseId);
+  //       console.log(groceryArray);
+  //       console.log("house id in GF", houseId)
+  //       let filteredGroceryArray = filterArrayByID(groceryArray, 'houseId', houseId);
+  //       resolve(filteredGroceryArray);
+  //       console.log("GF FGA", filteredGroceryArray)
+  //     }, (error) => {
+  //       console.log(error);
+  //       reject(error);
+  //     });
+  //   });
+  // };
+
   let getGroceryList = (houseId) => {
+    let groceryList = [];
     return $q( (resolve, reject) => {
-      $http.get(`${FirebaseURL}/grocery.json`)
-      .then((data) => {
-        let groceryArray = convertResultsToArray(data.data, 'homeid', houseId);
-        let filteredGroceryArray = filterArrayByID(groceryArray,'homeid', houseId);
-        resolve(filteredGroceryArray);
-      }, (error) => {
-        console.log(error);
+      $http.get(`${FirebaseURL}/grocery.json?orderBy="houseId"&equalTo="${houseId}"`)
+      .success( (groceryObj) => {
+        console.log("groceryObj before loop", groceryObj);
+        Object.keys(groceryObj).forEach((key) => {
+          groceryObj[key].id = key;
+          groceryList.push(groceryObj[key]);
+        });
+        resolve(groceryList);
+      })
+      .error( (error) => {
         reject(error);
       });
     });
