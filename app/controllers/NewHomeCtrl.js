d280807890b852944748c4849ff2382a13d74eb7
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-15 10:56:17 -0500

    houses now populate the DOM

--- app/controllers/NewHomeCtrl.js
+++ app/controllers/NewHomeCtrl.js
@@ -1,11 +1,11 @@
 app.controller("NewHomeCtrl", function($scope, $window, AuthFactory, $routeParams, HomeFactory) {
-  $scope.newUserObj = {
-      "firstName": "",
-      "lastName": "",
-      "uid": "",
-      "email": "",
-      "password": ""
-  };
+    $scope.newUserObj = {
+        "firstName": "",
+        "lastName": "",
+        "uid": "",
+        "email": "",
+        "password": ""
+    };
 
     $scope.account = {
         "email": "",
@@ -15,7 +15,7 @@ app.controller("NewHomeCtrl", function($scope, $window, AuthFactory, $routeParam
     $scope.homeInfo = {
         "address": "",
         "houseName": "",
-        "houseMemberUid": "",
+        "houseMemberUid": [],
         "streetAddress": "",
         "city": "",
         "state": "",
@@ -26,27 +26,27 @@ app.controller("NewHomeCtrl", function($scope, $window, AuthFactory, $routeParam
     $scope.registerUser = () => {
         console.log("register with me. Hail Mary!");
         AuthFactory.createUser({
-            email: $scope.account.email,
-            password: $scope.account.password,
-            userName: $scope.account.userName
-        })
-        .then((userData) => {
-          $scope.goMore = true;
-        }, (error) => {
-            console.log("Error Creating home and new User");
-        });
+                email: $scope.account.email,
+                password: $scope.account.password,
+                userName: $scope.account.userName
+            })
+            .then((userData) => {
+                $scope.goMore = true;
+            }, (error) => {
+                console.log("Error Creating home and new User");
+            });
     };
 
     $scope.createUserFb = () => {
-      AuthFactory.createUserFb({
-        "email": $scope.account.email,
-        "password": $scope.account.password,
-        "firstName": $scope.newUserObj.firstName,
-        "lastName": $scope.newUserObj.lastName,
-        "uid": AuthFactory.getUid()
-      }).then( function(userData) {
-        $scope.loginRegisteredUser();
-      })
+        AuthFactory.createUserFb({
+            "email": $scope.account.email,
+            "password": $scope.account.password,
+            "firstName": $scope.newUserObj.firstName,
+            "lastName": $scope.newUserObj.lastName,
+            "uid": AuthFactory.getUid()
+        }).then(function(userData) {
+            $scope.loginRegisteredUser();
+        })
     };
 
 
@@ -65,48 +65,59 @@ app.controller("NewHomeCtrl", function($scope, $window, AuthFactory, $routeParam
     };
 
     $scope.registerNewHome = () => {
-      console.log("Register a new home with me. Hail Mary");
-      // $scope.homeInfo.houseMemberUid: AuthFactory.getUid();
-      HomeFactory.createHome({
-        "streetAddress": $scope.homeInfo.streetAddress,
-        "houseName": $scope.homeInfo.houseName,
-        "houseMemberUid": AuthFactory.getUid(),
-        "city": $scope.homeInfo.city,
-        "state": $scope.homeInfo.state,
-        "zipCode": $scope.homeInfo.zipCode,
-        "password": $scope.homeInfo.password
-      });
+        console.log("Register a new home with me. Hail Mary");
+        // $scope.homeInfo.houseMemberUid: AuthFactory.getUid();
+        HomeFactory.createHome({
+            "streetAddress": $scope.homeInfo.streetAddress,
+            "houseName": $scope.homeInfo.houseName,
+            "houseMemberUid": [AuthFactory.getUid()],
+            "city": $scope.homeInfo.city,
+            "state": $scope.homeInfo.state,
+            "zipCode": $scope.homeInfo.zipCode,
+            "password": $scope.homeInfo.password
+        });
     };
 
 
 
     $scope.login = () => {
-      console.log("YOURE GOING TO LOGIN");
-      AuthFactory.loginUser($scope.account)
-      .then( (data) => {
-        if (data) {
-          HomeFactory.getUsersHome(AuthFactory.getUid())
-          .then( function() {
-            $window.location.href= `#/home-tools/${HomeFactory.getHouseid()}`;
-          });
-        } else {
-          $window.location.href = "#/";
-        }
-      }, (error) => {
-        console.log("YOU HAVE AN ERROR");
-      });
+        console.log("YOURE GOING TO LOGIN");
+        AuthFactory.loginUser($scope.account)
+            .then((data) => {
+                if (data) {
+                    HomeFactory.getUsersHome(AuthFactory.getUid())
+                        .then(function() {
+                            $window.location.href = `#/home-tools/${HomeFactory.getHouseid()}`;
+                        });
+                } else {
+                    $window.location.href = "#/";
+                }
+            }, (error) => {
+                console.log("YOU HAVE AN ERROR");
+            });
     };
 });
 
 app.controller("SearchCtrl", function($scope, $window, AuthFactory, $routeParams, HomeFactory) {
-  $scope.homeSearch = {
-    "zipCode": ""
-  };
+    $scope.homeSearch = {
+        "zipCode": ""
+    };
+    $scope.homeList = [];
 
-  $scope.searchByZip = (zipCode) => {
-    HomeFactory.searchByZip(zipCode)
-    .then( (data) => {
-      console.log(data);
-    });
-  }
+    $scope.searchByZip = (zipCode) => {
+         HomeFactory.searchByZip(zipCode).then( function(homeList) {
+            $scope.homeList = homeList;
+            console.log("$scope.homeList", $scope.homeList);
+        })
+    };
+
+    // $scope.getGroceryList = function() {
+    //     GroceryFactory.getGroceryList(_homeid).then(function(filteredGroceryArray) {
+    //         $scope.purchasedList =   GroceryFactory.filtergroceryList(filteredGroceryArray, 'purchased', true);
+    //         console.log("purchasedlist", $scope.purchasedList)
+    //         $scope.groceryList = GroceryFactory.filtergroceryList(filteredGroceryArray, 'purchased', false);
+    //     });
+    // };
+
+
 })
