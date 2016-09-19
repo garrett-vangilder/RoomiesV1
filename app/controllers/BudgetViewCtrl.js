576cfab228460bde62cea30b2e50719c21a0f933
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-19 12:49:55 -0500

    budget items can be added and they now read to the DOM

--- app/controllers/BudgetViewCtrl.js
+++ app/controllers/BudgetViewCtrl.js
@@ -1,3 +1,25 @@
-app.controller("BudgetViewCtrl", function() {
-  console.log("hello!")
+app.controller("BudgetViewCtrl", function($scope, $window, $routeParams, BudgetFactory, HomeFactory, AuthFactory) {
+    let _homeid = $routeParams.homeid;
+
+    $scope.newBudgetItem = {
+        "categoryName": "",
+        "amount": null,
+        "houseId": _homeid,
+        "currentAmountSpent": 0
+    };
+
+    $scope.addBudgetItem = function() {
+        console.log('sending over new budget item to FB')
+        BudgetFactory.newBudgetItem($scope.newBudgetItem)
+            .then(function() {
+                $scope.getBudgetList();
+            })
+    }
+
+    $scope.getBudgetList = function() {
+      BudgetFactory.getBudgetList(_homeid).then( function(filteredBudgetArray) {
+        $scope.budgetItems = filteredBudgetArray;
+      })
+    }
+
 });
