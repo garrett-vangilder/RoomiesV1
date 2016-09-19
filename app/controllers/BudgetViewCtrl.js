6898ca38af2b60d492fd9b1f34f8f411e74c13f4
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-19 13:19:59 -0500

    can edit budget categories

--- app/controllers/BudgetViewCtrl.js
+++ app/controllers/BudgetViewCtrl.js
@@ -8,6 +8,27 @@ app.controller("BudgetViewCtrl", function($scope, $window, $routeParams, BudgetF
         "currentAmountSpent": 0
     };
 
+    $scope.selectedBudgetItem = {
+      "categoryName": "",
+      "amount": ""
+    };
+
+    $scope.newExpenseItem = {
+      "paidTo": "",
+      "amount": null,
+      "dateDue": "",
+      "category": "",
+      "notes": ""
+    };
+
+    $scope.selectedExpenseItem = {
+      "paidTo": "",
+      "amount": null,
+      "dateDue": "",
+      "category": "",
+      "notes": ""
+    };
+
     $scope.addBudgetItem = function() {
         console.log('sending over new budget item to FB')
         BudgetFactory.newBudgetItem($scope.newBudgetItem)
@@ -16,10 +37,31 @@ app.controller("BudgetViewCtrl", function($scope, $window, $routeParams, BudgetF
             })
     }
 
+    $scope.deleteBudgetItem = function(itemId) {
+      console.log('deleting an item from the budget category list')
+      BudgetFactory.deleteBudgetItem(itemId).then(function() {
+        $scope.getBudgetList();
+      })
+    }
+    $scope.editBudgetItem = function(itemId) {
+      $scope.isEdit = false;
+      BudgetFactory.getSingleBudgetItem(itemId).then(function(budgetObj) {
+        console.log('budgetObj', budgetObj);
+        budgetObj.categoryName = $scope.selectedBudgetItem.categoryName;
+        budgetObj.amount = $scope.selectedBudgetItem.amount;
+        BudgetFactory.updateBudgetItem(itemId,budgetObj).then(function(newObj) {
+          console.log('should be new item', newObj)
+          $scope.getBudgetList();
+        })
+      })
+    }
+
+
     $scope.getBudgetList = function() {
       BudgetFactory.getBudgetList(_homeid).then( function(filteredBudgetArray) {
         $scope.budgetItems = filteredBudgetArray;
       })
     }
+
 
 });
