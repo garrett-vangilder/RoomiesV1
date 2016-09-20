9bbcac031e127502a151eb776dcaf5899c61b26f
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-19 20:25:12 -0500

    working on navbar

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -1,12 +1,43 @@
-app.controller("NavBarCtrl", function($scope, $location, $routeParams) {
-    $scope.navItems = [{
-      url: "#/register",
-      name: "Register",
-      showState: "!$parent.isLoggedIn"
-    }, {
-      url: "#/home-tools/" + $routeParams.houseId,
-      name: "Home Tools",
-      showState: "$parent.isLoggedIn"
-    }]
+app.controller("NavBarCtrl", function($scope, $location,  $window, $routeParams, AuthFactory) {
+  let homeId = $scope.getHouseid();
+  let homeIdParams = $routeParams.homeid;
+  let uid = $scope.getUser();
+
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log('creating navbar')
+    if (user) {
+      $scope.navItems = [{
+        url: "#/registeruser",
+        name: "Register",
+        showState: "!$parent.isLoggedIn"
+      }, {
+        url: `#/home-tools/${$routeParams.homeid}`,
+        name: "Home Tools",
+        showState: "$parent.isLoggedIn"
+      }, {
+        url: `#/messages/${AuthFactory.getUid()}`,
+        name: "Messages",
+        showState: "$parent.isLoggedIn"
+      }]
+    } else {
+        $scope.navItems = [];
+    }
+  })
+
+    // $scope.navItems = [{
+    //   url: "#/registeruser",
+    //   name: "Register",
+    //   showState: "!$parent.isLoggedIn"
+    // }, {
+    //   url: `#/home-tools/${$routeParams.homeid}`,
+    //   name: "Home Tools",
+    //   showState: "$parent.isLoggedIn"
+    // }, {
+    //   url: `#/messages/${AuthFactory.getUid()}`,
+    //   name: "Messages",
+    //   showState: "$parent.isLoggedIn"
+    // }]
+
       $scope.isActive = (viewLocation) => viewLocation === $location.path();
 });
--- app/controllers/TopCtrl.js
+++ app/controllers/TopCtrl.js
@@ -3,12 +3,19 @@
 app.controller("TopCtrl", function($scope, $location, $window, AuthFactory) {
   $scope.isLoggedIn = false;
   let currentUser = null;
+  let currentHouseid = null
+  $scope.currentHouseid = null
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       currentUser = user.uid;
       $scope.isLoggedIn = true;
       console.log("Current user logged in", user.uid)
+      AuthFactory.getUsersHomeId(currentUser).then( function(filteredHome) {
+        currentHouseid = filteredHome
+        $scope.currentHouseid = filteredHome
+        console.log("currentHouseid is ", currentHouseid)
+      })
     } else {
       currentUser = null;
       $scope.isLoggedIn = false;
@@ -20,6 +27,10 @@ app.controller("TopCtrl", function($scope, $location, $window, AuthFactory) {
   $scope.getUser = function() {
     return currentUser
   };
+
+  $scope.getHouseid = function() {
+    return currentHouseid
+  }
 
   $scope.logout = function() {
     AuthFactory.logoutUser()
