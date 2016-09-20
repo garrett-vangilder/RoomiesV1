ca2cb4d7944811ce964863f1d5a29f9462f90293
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-20 16:44:16 -0500

    radio buttons for each house member

--- app/controllers/ChoresViewCtrl.js
+++ app/controllers/ChoresViewCtrl.js
@@ -1,45 +1,58 @@
-app.controller("ChoresViewCtrl", function($scope, $window,$routeParams, ChoresFactory,HomeFactory, AuthFactory) {
-  let _homeid = $routeParams.homeid;
-  let _uid = AuthFactory.getUid();
+app.controller("ChoresViewCtrl", function($scope, $window, $routeParams, ChoresFactory, HomeFactory, AuthFactory) {
+    let _homeid = $routeParams.homeid;
+    let _uid = AuthFactory.getUid();
+    $scope.roommateList = [];
 
-  $scope.newChoreItem = {
-    "task": "",
-    "dueDate": "",
-    "assignedTo": "",
-    "uid": _uid,
-    "houseId": _homeid,
-    "completed": false
-  };
+    $scope.newChoreItem = {
+        "task": "",
+        "dueDate": "",
+        "assignedTo": "",
+        "uid": _uid,
+        "houseId": _homeid,
+        "completed": false
+    };
 
-  $scope.addChoreItem = function() {
-    ChoresFactory.newChore($scope.newChoreItem)
-    .then( function() {
-      $scope.getChoresList();
-    });
-  };
+    $scope.addChoreItem = function() {
+        ChoresFactory.newChore($scope.newChoreItem)
+            .then(function() {
+                $scope.getChoresList();
+            });
+    };
 
-  $scope.getChoresList = function() {
-    ChoresFactory.getChoresList(_homeid).then( function(filteredChoresArray) {
-      $scope.chores = filteredChoresArray;
-    });
-  };
+    $scope.getChoresList = function() {
+        ChoresFactory.getChoresList(_homeid).then(function(filteredChoresArray) {
+            $scope.chores = filteredChoresArray;
+        });
+    };
 
-  $scope.completedTask =function(itemId) {
-    ChoresFactory.getSingleChoreItem(itemId).then( function(choreObj) {
-      console.log(choreObj)
-      choreObj.completed = true;
-      ChoresFactory.patchChoreItem(itemId, choreObj).then( function() {
-        $scope.getChoresList();
-      })
-    })
-  };
+    $scope.completedTask = function(itemId) {
+        ChoresFactory.getSingleChoreItem(itemId).then(function(choreObj) {
+            console.log(choreObj)
+            choreObj.completed = true;
+            ChoresFactory.patchChoreItem(itemId, choreObj).then(function() {
+                $scope.getChoresList();
+            })
+        })
+    };
 
-  $scope.deleteItem = function(itemId) {
-    ChoresFactory.deleteChoreItem(itemId).then( function(choreObj) {
-      console.log(choreObj);
-      $scope.getChoresList();
+    $scope.deleteItem = function(itemId) {
+        ChoresFactory.deleteChoreItem(itemId).then(function(choreObj) {
+            console.log(choreObj);
+            $scope.getChoresList();
 
-    })
-  };
+        })
+    };
+
+
+    $scope.getRoommateList = function() {
+        HomeFactory.getSingleHome($routeParams.homeid).then(function(obj) {
+            console.log("obj", obj);
+            angular.forEach(obj.houseMemberUid, function(value) {
+                AuthFactory.getUsersFirstName(value).then(function(filteredName) {
+                    $scope.roommateList.push(filteredName)
+                })
+            })
+        })
+    }
 
 });
